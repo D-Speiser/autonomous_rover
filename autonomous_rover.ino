080b73735ed37297a884962c06e01ec0b5b2eb3b
Daniel Speiser <DanielSpeiser93@gmail.com>
2016-01-21 00:25:29 -0500

    add basic gyro/accel functionality. TODO: use gyro to detect change in angle for calibration

--- autonomous_rover.ino
+++ autonomous_rover.ino
@@ -1,6 +1,10 @@
 #include <Servo.h> 
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include "Wire.h"
 
 Servo servo;
+MPU6050 mpu;
 const int servoPin = 10;
 const int trigPin  = 11;
 const int echoPin  = 12;
@@ -9,6 +13,103 @@ const int rightMotor [] = {6, 7};
 
 long * distances180 = new long [5];
 long * distances360 = new long [8];
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+float * mpuData;
+float currentAngle;
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+  mpuInterrupt = true;
+}
+
+void mpuSetup() {
+  TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz). Comment this line if having compilation difficulties with TWBR.
+  mpu.initialize();
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+  // load and configure the DMP
+  devStatus = mpu.dmpInitialize();
+
+  mpu.setXGyroOffset(49);
+  mpu.setYGyroOffset(42);
+  mpu.setZGyroOffset(24);
+  mpu.setXAccelOffset(-1316);
+  mpu.setYAccelOffset(577);
+  mpu.setZAccelOffset(1335);
+  
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+    attachInterrupt(0, dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
+}
+
+float * getMPUData() {
+  // clear extra packets between calls
+  mpu.resetFIFO();
+  // wait for MPU interrupt or extra packet(s) available
+  while (!mpuInterrupt && fifoCount < packetSize) {}
+  // reset interrupt flag and get INT_STATUS byte
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+  // get current FIFO count
+  fifoCount = mpu.getFIFOCount();
+  // check for overflow (this should never happen unless our code is too inefficient)
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    // reset so we can continue cleanly
+    mpu.resetFIFO();
+    Serial.println(F("FIFO overflow!"));
+  // otherwise, check for DMP data ready interrupt (this should happen frequently)
+  } else if (mpuIntStatus & 0x02) {
+    // wait for correct available data length, should be a VERY short wait
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+    // read a packet from FIFO
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+    // track FIFO count here in case there is > 1 packet available
+    // (this lets us immediately read more without waiting for an interrupt)
+    fifoCount -= packetSize;
+    // display Euler angles in degrees
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+  }
+  // convert from rads to degrees
+  for (int i = 0; i < 3; i++) {
+    ypr[i] *= 180/M_PI;
+  }
+  return ypr;
+}
 
 void calibrateRover() {
   distances360 = get360Distances();
@@ -33,6 +134,7 @@ long * get360Distances() {
   }
 
   right();
+  // TODO: Replace timed rotation angle using rpm by gyro/accel
   delay(450);
   stop();
   
@@ -126,7 +228,9 @@ void stop() {
 }
 
 void setup() {
-  Serial.begin(9600);
+  Wire.begin();
+  Serial.begin(115200);
+  mpuSetup();
   servo.attach(servoPin);
   pinMode(echoPin, INPUT);
   pinMode(trigPin, OUTPUT);
@@ -139,6 +243,10 @@ void setup() {
 }
 
 void loop() {
-  
+  // if programming failed, don't try to do anything
+  if (!dmpReady) {
+    stop();
+    return;
+  }
 }
 
